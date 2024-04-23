4df3c790f59279297018715d8abc3b5f9caa7ebc
Katriel Chiu <katriel_chiu@msn.com>
2024-04-23 00:37:48 -0700

    added comments to the code

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const shapes = require('./lib/shapes.js');
+
+// Additional package I found from NPM which helps with color input validation
 const validateColor = require('validate-color').default;
 
+// Create an array of questions for user input with validation for some questions
 const questions = [
     {
         type: "input",
@@ -57,18 +61,25 @@ const questions = [
     }
 ];
 
+// Function to write a SVG file
 function writeToFile(fileName, data) {
+    // Write our data to a file with a given name
     fs.writeFile(fileName, data, (err) =>
         err ? console.error(err) : console.log("Logo successfully generated!")
     );
 }
 
+// Function to initialize app
 function init() {
+    // Use our inquirer to prompt user with questions
     inquirer.prompt(questions)
     .then((response) => {
         console.log("Generating logo ...");
+
+        // Generate the specified shape with the inputted parameters
         const shape = shapes.createShapeObject(response);
 
+        // Our SVG string with proper indentation - we use trim to remove the extra line
         const svg = `
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
     ${shape.render()}
@@ -79,4 +90,5 @@ function init() {
     });
 }
 
+// Function call to initialize app
 init();
